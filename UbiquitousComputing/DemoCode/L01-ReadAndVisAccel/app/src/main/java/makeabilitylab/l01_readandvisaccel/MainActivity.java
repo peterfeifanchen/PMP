ff681dc5899405c9f1fa89646d9386efced5afaa
Peter Chen <peterchen@Peters-MacBook-Air.local>
2020-03-05 14:58:24 -0800

    Ubiquitous Computing

---
+++ UbiquitousComputing/DemoCode/L01-ReadAndVisAccel/app/src/main/java/makeabilitylab/l01_readandvisaccel/MainActivity.java
@@ -0,0 +1,18 @@
+package makeabilitylab.l01_readandvisaccel;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorManager;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
